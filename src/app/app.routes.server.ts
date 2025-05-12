4b995c3b521efaaf8422a31319c08ab27b485154
Marco Pollacci <pollacci@gmail.com>
2025-05-12 16:41:33 +0200

    build(project): :bug: fix server route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,28 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+const MOCKED_EVENTS = [
+  {
+    eventId: '1',
+  },
+];
+
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'events/add-rating/:eventId',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return MOCKED_EVENTS;
+    },
+  },
+  {
+    path: 'events/single-events/:eventId',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return MOCKED_EVENTS;
+    },
   },
 ];
