cb032a5c1c6412cd92b3937305bc02314a2fada7
Marco Pollacci <pollacci@gmail.com>
2025-05-27 08:33:35 +0200

    fix(server-side): :bricks: better typing

--- src/server/helpers/api.helper.ts
+++ src/server/helpers/api.helper.ts
@@ -1,10 +1,10 @@
 export const executeApiCall = async <T>(
-  funcToCall: any
+  funcToCall: Function
 ): Promise<{ status: number; message: T | string }> => {
   let status = 200;
-  let message;
+  let message: T | string;
   try {
-    message = await funcToCall.call();
+    message = await funcToCall.call(undefined);
   } catch (error) {
     status = 500;
     message = 'KO';
